f39fd8b909fdfecb713479c4bb8d7a598489ed86
donghakim <donghakim@donghakimui-MacBookPro.local>
2022-01-11 20:20:56 +0900

    first commit

---
+++ data.js
@@ -0,0 +1,8 @@
+export const users = [
+  { id: 1, name: "ID", age: 30 },
+  { id: 2, name: "BJ", age: 32 },
+  { id: 3, name: "JM", age: 36 },
+  { id: 4, name: "PJ", age: 10 },
+  { id: 5, name: "HA", age: 15 },
+  { id: 6, name: "JR", age: 18 },
+];
