4f939d5079442c45cf5d34fc8310c21e6206eef5
dongha1992 <kamdunga2002@gmail.com>
2022-01-23 16:46:45 +0900

    목데이터 추가

--- data.js
+++ data.js
@@ -7,3 +7,25 @@ export const users = [
   { id: 6, name: "JR", age: 18 },
   { id: 20, name: "JRA", age: 40 },
 ];
+
+export const posts = [
+  { id: 201, body: "내용1", userId: 1 },
+  { id: 202, body: "내용2", userId: 2 },
+  { id: 203, body: "내용3", userId: 3 },
+  { id: 204, body: "내용4", userId: 4 },
+  { id: 205, body: "내용5", userId: 5 },
+  { id: 206, body: "내용6", userId: 20 },
+  { id: 207, body: "내용7", userId: 1 },
+  { id: 208, body: "내용8", userId: 2 },
+];
+export const comments = [
+  { id: 301, body: "댓글1", userId: 1, postId: 201 },
+  { id: 302, body: "댓글2", userId: 2, postId: 202 },
+  { id: 303, body: "댓글3", userId: 3, postId: 203 },
+  { id: 304, body: "댓글4", userId: 4, postId: 204 },
+  { id: 305, body: "댓글5", userId: 5, postId: 205 },
+  { id: 306, body: "댓글6", userId: 20, postId: 201 },
+  { id: 307, body: "댓글7", userId: 1, postId: 201 },
+  { id: 308, body: "댓글8", userId: 2, postId: 202 },
+  { id: 309, body: "댓글9", userId: 3, postId: 201 },
+];
