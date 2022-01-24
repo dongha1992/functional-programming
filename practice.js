1d507062687606418592d568e324db8aa6880b24
dongha1992 <kamdunga2002@gmail.com>
2022-01-24 20:52:59 +0900

    userByComment 함수 추출

--- practice.js
+++ practice.js
@@ -4,6 +4,13 @@ const commentsByPostId = _pipe(_pluck("id"), (postIds) => {
   });
 });
 
+const postsByUserId = _pipe(
+  _filter(posts, (post) => {
+    return post.userId === 1;
+  }),
+  commentsByPostId
+);
+
 // 1. 특정인의 posts의 모든 comments 거르기
 
 _go(
@@ -15,16 +22,20 @@ _go(
 
 // 2. 특정인의 posts에 comments를 단 친구의 이름 뽑기
 
-_go(
-  _filter(posts, (post) => {
-    return post.userId === 1;
-  }),
-  commentsByPostId,
+const userByComment = _pipe(
   _map((comment) => {
     return _find(users, (user) => {
       return user.id === comment.userId;
     });
   })
+);
+
+_go(
+  _filter(posts, (post) => {
+    return post.userId === 1;
+  }),
+  commentsByPostId,
+  userByComment
 );
 
 // 3. 특정인의 posts에 comments 단 친구들 카운트 정보
@@ -34,11 +45,7 @@ _go(
     return post.userId === 1;
   }),
   commentsByPostId,
-  _map((comment) => {
-    return _find(users, (user) => {
-      return user.id === comment.userId;
-    });
-  }),
+  userByComment,
   _countBy((user) => user.name)
 );
 
