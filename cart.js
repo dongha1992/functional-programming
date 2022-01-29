a55ff361220cf530e14ad6a26ae97e13baf49002
dongha1992 <kamdunga2002@gmail.com>
2022-01-29 18:54:05 +0900

    cart 완료

--- cart.js
+++ cart.js
@@ -17,6 +17,51 @@ _reduce(
 
 // 2. 선택된 아이템 총 수량
 
+_reduce(
+  products,
+  (tq, product) => {
+    if (product.isSelected) {
+      return _reduce(
+        product.sizes,
+        (tq, size) => {
+          return tq + size.quantity;
+        },
+        tq
+      );
+    }
+    return tq;
+  },
+  0
+);
+
 // 3. 모든 가격
 
+_reduce(
+  products,
+  (tp, product) => {
+    return _reduce(
+      product.sizes,
+      (tp, size) => {
+        return tp + size.quantity * size.price;
+      },
+      tp
+    );
+  },
+  0
+);
+
 // 4. 선택된 아이템 총 가격
+
+_reduce(
+  products,
+  (tp, product) => {
+    return _reduce(
+      product.sizes,
+      (tp, size) => {
+        return tp + size.quantity * size.price;
+      },
+      tp
+    );
+  },
+  0
+);
